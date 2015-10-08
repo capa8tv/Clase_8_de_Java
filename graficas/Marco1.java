732daf3b42226645b13ca320f07d1bd32b3ac2a6
Capa8.tv <canalcapa8@gmail.com>
2015-10-07 22:42:10 -0400

    Marco1.java

---
+++ graficas/Marco1.java
@@ -0,0 +1,38 @@
+package graficas;
+import java.awt.*;
+import javax.swing.*;
+
+public class Marco1 extends JFrame 
+{
+    
+    public Marco1()
+    {
+       
+        
+    }
+    public void getApp()
+    {
+        //setSize(1024,768);
+        //setLocation(200,50);
+        setBounds(200,50,1024,768);
+        setVisible(true);
+        setTitle("..:: Hola título desde www.capa8.tv Ñandú ::..");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+    public void getAppCentrada()
+    {
+        Toolkit mipantalla=Toolkit.getDefaultToolkit();
+        Dimension tamanoPantalla=mipantalla.getScreenSize();
+        setSize(tamanoPantalla.width/2,tamanoPantalla.height/2);
+        setLocation(tamanoPantalla.width/4,tamanoPantalla.height/4);
+        setVisible(true);
+        setTitle("..:: Hola título desde www.capa8.tv Ñandú ::..");
+        setResizable(true);
+        
+        Image icono=mipantalla.getImage("src/images/cesar.png");
+        setIconImage(icono);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
