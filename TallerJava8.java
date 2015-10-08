069565c6978afc8f404371358260ee18d564dfaa
Capa8.tv <canalcapa8@gmail.com>
2015-10-07 22:41:37 -0400

    TallerJava8.java

---
+++ TallerJava8.java
@@ -0,0 +1,15 @@
+
+package tallerjava8;
+
+import graficas.Marco1;
+
+public class TallerJava8 {
+
+    
+    public static void main(String[] args) 
+    {
+       Marco1 marco1=new Marco1();
+       marco1.getAppCentrada();
+    }
+    
+}
